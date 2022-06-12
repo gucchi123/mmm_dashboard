638336bf6b4b05df4aaadcac7d39fd0b0a285410
gucchi123 <suikou0@gmail.com>
2022-06-13 00:56:21 +0900

    Add files via upload

--- mmm.py
+++ mmm.py
@@ -260,7 +260,6 @@ def main():
         )
 
         if selected_gender == "東京男性":
-            for trial in range(150000, 1550000, 20000):
                 st.header("＜{}＞投資金額増減時におけるチャネル最適投資金額によるCVの変動分析".format(selected_gender))
                 st.subheader("現状とシミュレーションとしての投資金額(*)の増減割合")
                 st.write("（*）150,000円 - 1,530,000円")
@@ -272,6 +271,8 @@ def main():
                 st.write("")
                 st.write('-----------------------------------------------------------------------')
                 st.subheader("投資金額毎の分析結果")
+
+            for trial in range(150000, 1550000, 20000):
                 if trial==150000:
                     st.write("１日の広告投資金額の総額{:,}円".format(trial))
                     st.image("https://raw.githubusercontent.com/gucchi123/male_mmm_data/main/invest_simu/{}_reallocated_respo{}.png".format(ma_ping, trial))
